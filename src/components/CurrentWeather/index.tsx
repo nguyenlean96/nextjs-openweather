b20e15d39215182460a5ed1cedb184b56ba21eaf
Le An Nguyen <nguyenlean96@gmail.com>
2024-09-26 15:01:58 -0400

    Added current weather and forecast weather placeholder components

---
+++ src/components/CurrentWeather/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState, useRef } from "react";
+import { useWeatherContext } from "@/context/WeatherDataProvider";
+import { useDebounce, useInfiniteLoading } from "@/hooks";
+
+import React from 'react'
+
+export default function CurrentWeather() {
+  return (
+    <div>CurrentWeather</div>
+  )
+}
