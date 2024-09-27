1c99cd5792daa1f8249904738796eecdcb01606c
Le An Nguyen <nguyenlean96@gmail.com>
2024-09-27 11:55:12 -0400

    Added mantime to manage window size change events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import "@/styles/fog.css";
 import type { AppProps } from "next/app";
+import { useViewportSize } from '@mantine/hooks';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { height, width } = useViewportSize();
+  return (
+    <Component
+      {...pageProps}
+      height={height}
+      width={width}
+    />
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import WeatherProvider from "@/context/WeatherDataProvider";
-import { Main } from "@/components";
+import { Main, SearchPanel } from "@/components";
 
-export default function Home() {
-  
+export default function Home({ width, height, props }: { width: number; height: number; props: any }) {
   return (
     <WeatherProvider>
-      <Main />
+      <div className="grid grid-cols-4">
+        <SearchPanel
+          width={width}
+          height={height}
+        />
+        <Main />
+      </div>
     </WeatherProvider>
   );
 }
