87bbe66b0fb8856722ce6f18059abe4e023bdee5
Le An Nguyen <nguyenlean96@gmail.com>
2024-09-27 12:13:16 -0400

    Removed unused dependencies

--- src/components/CurrentWeather/index.tsx
+++ src/components/CurrentWeather/index.tsx
@@ -1,7 +1,3 @@
-import { useEffect, useState, useRef } from "react";
-import { useWeatherContext } from "@/context/WeatherDataProvider";
-import { useDebounce, useInfiniteLoading } from "@/hooks";
-
 import React from 'react'
 
 export default function CurrentWeather() {
