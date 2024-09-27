bd9cb9ba5c50b4372f35bd540d92a0646c13f28a
Le An Nguyen <nguyenlean96@gmail.com>
2024-09-27 11:57:13 -0400

    Added fog effect

---
+++ src/components/fog-effect/fog-background-effect/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from 'react'
+import Fog1 from '../graphic/fog1.png';
+import Fog2 from '../graphic/fog2.png';
+import Fog3 from '../graphic/fog3.png';
+import Fog4 from '../graphic/fog4.png';
+import Fog5 from '../graphic/fog5.png';
+
+export default function FogBackgroundEffect() {
+  return (
+    <div className="fog absolute top-0 left-0 right-0 w-full z-30">
+      <Image src={Fog1} alt="fog" className="fog1" />
+      <Image src={Fog2} alt="fog" className="fog2" />
+      <Image src={Fog3} alt="fog" className="fog3" />
+      <Image src={Fog4} alt="fog" className="fog4" />
+      <Image src={Fog5} alt="fog" className="fog5" />
+      <Image src={Fog1} alt="fog" className="fog10" />
+      <Image src={Fog2} alt="fog" className="fog9" />
+      <Image src={Fog3} alt="fog" className="fog8" />
+      <Image src={Fog4} alt="fog" className="fog7" />
+      <Image src={Fog5} alt="fog" className="fog6" />
+    </div>
+  )
+}
