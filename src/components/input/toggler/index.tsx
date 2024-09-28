1a176715199369cf0e0f8380aa4ecd21c5291310
Le An Nguyen <nguyenlean96@gmail.com>
2024-09-28 16:27:27 -0400

    Fixed wrong active bg color

--- src/components/input/toggler/index.tsx
+++ src/components/input/toggler/index.tsx
@@ -6,23 +6,11 @@ export default function Toggler({
   onClick: () => void;
 }) {
   return (
-    /** 
-     * peer-checked:after:translate-x-full 
-     * rtl:peer-checked:after:-translate-x-full
-     * peer-checked:after:border-white
-     * peer-checked:bg-blue-600
-     */
-
     <label className="inline-flex items-center cursor-pointer">
-      <input aria-label="Toggle" type="checkbox" value="" className="sr-only peer" onClick={() => onClick()} />
-      <div className={`
-        h-6 w-11 relative flex items-center rounded-full bg-gray-200 dark:bg-gray-700 peer px-0.5
-      
-      ${value ? 'bg-blue-600' : 'hover:bg-blue-100 bg-gray-200'}`}
+      <input type="checkbox" value="" className="sr-only peer" onClick={() => onClick()} />
+      <div className={'h-6 w-11 relative flex items-center rounded-full dark:bg-gray-700 peer px-0.5 ' + (value ? 'bg-blue-500' : 'hover:bg-blue-100 bg-gray-200')}
       >
-        <div className={`h-5 w-5 rounded-full bg-white border border-gray-300 dark:border-gray-600 transition-all
-          ${value ? 'translate-x-full' : '-translate-x-0'}
-          `}
+        <div className={'h-5 w-5 rounded-full bg-white border border-gray-300 dark:border-gray-600 transition-all ' + (value ? 'translate-x-full' : '-translate-x-0')}
         ></div>
       </div>
     </label>
