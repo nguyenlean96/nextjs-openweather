7602d43b7cd3047175b9fa1823cd80a061094176
Le An Nguyen <nguyenlean96@gmail.com>
2024-09-27 20:13:12 -0400

    Fixed deprecated dir

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import Main from './main';
 import SearchPanel from './search-panel';
-import WeatherForecast from './weather-forecast';
+import { HourlyForecast, DailyForecast } from './weather-forecast';
 
 export {
   Main,
   SearchPanel,
-  WeatherForecast
+  HourlyForecast,
+  DailyForecast
 };
