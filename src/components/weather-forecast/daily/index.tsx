8b543b2546841210a30a801f36a8bd2ee9e1cacf
Le An Nguyen <nguyenlean96@gmail.com>
2024-10-01 18:17:07 -0400

    Removed unused import

--- src/components/weather-forecast/daily/index.tsx
+++ src/components/weather-forecast/daily/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { m, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { getDayOfWeek } from '@/util';
 import { useWeatherContext } from '@/context/WeatherDataProvider';
 
